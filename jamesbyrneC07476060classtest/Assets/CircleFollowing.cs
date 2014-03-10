ec74becfb65ef00cacb5867c1e26dee7f6b2bb91
Jamesbyrne117 <james.swords90@gmail.com>
2014-03-10 21:06:06 +0000

    added project

---
+++ jamesbyrneC07476060classtest/Assets/CircleFollowing.cs
@@ -0,0 +1,39 @@
+﻿/*using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CircleFollowing : MonoBehaviour {
+	float mass = 1f;
+	float maxSpeed = 5f;
+//	Vector3 force = Vector3.forward;
+	//public Vector3 velocity = (0,0,5);
+	Vector3 velocity = Vector3.zero;
+	int currentWaypoint = 1;
+	ArrayList waypoints;
+
+	// Use this for initialization
+	void Start () 
+	{
+		velocity = Vector3 (0, 0, 5);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	for (int i = 0; i <1 ; i++) 
+		{
+			float ChangePosition = 36;
+
+			if (i > ChangePosition )
+			{
+				velocity = Vector3(Mathf.Sin(i)*10 , 0 , Mathf.Cos(i)*10);
+				ChangePosition = ChangePosition + 36;
+			}
+
+		}
+
+	
+	}
+}
+*/
---
+++ jamesbyrneC07476060classtest/Assets/boxFollow.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boxFollow : MonoBehaviour {
+
+	Vector3 velocity = Vector3.zero;
+	Vector3 force = Vector3.zero;
+	float maxSpeed = 5f;
+	float mass = 1f;
+	public Vector3 Target = Vector3.zero;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		Vector3 distance = transform.position - Target;
+		Debug.Log (distance);
+		velocity = distance / Time.deltaTime;
+
+
+
+		transform.position = transform.position + Target + velocity;
+	
+		Debug.DrawLine (transform.position, Target, Color.blue);
+	}
+
+}
